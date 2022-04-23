29b19d826ee0e5bac3e04d80ae7fbb0d8a450199
antemaz <antenehmaz@gmail.com>
2022-04-23 09:39:13 -0400

    test1

---
+++ src/main/java/amazon/BestSellersPage.java
@@ -0,0 +1,19 @@
+package amazon;
+
+import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class BestSellesPage extends BasePage {
+    private static final String URL = "https://www.amazon.com/";
+
+    @FindBy(xpath = "//img[@src='https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/BROWSE/L0/desktop/L0_DT_HERO_01W-v2.jpg']")
+    public WebElement mainFashionImage;
+
+    public BestSellesPage(){
+        webDriverWait.until(ExpectedConditions.urlContains(URL));
+        PageFactory.initElements(driver, this);
+    }
+}
